3231c2e501b8e22033734403d0b035bba389db01
Géry Debongnie <ged@odoo.com>
2021-08-01 21:40:32 +0200

    wip

--- src/b_node.ts
+++ src/b_node.ts
@@ -1,15 +1,7 @@
-import type { App } from "./app";
+// import type { App } from "./app";
 import { Block } from "./bdom";
-import type { Component } from "./component";
-import {
-  Fiber,
-  makeChildFiber,
-  makeRootFiber,
-  MountFiber,
-  RootFiber,
-  __internal__destroyed,
-} from "./fibers";
-import { STATUS } from "./status";
+import { mountBlock } from "./bdom/block";
+import type { Component, ComponentClosure } from "./index";
 import { toClassObj } from "./template_utils";
 
 // -----------------------------------------------------------------------------
@@ -24,120 +16,109 @@ export function getCurrent(): BNode | null {
 
 type LifecycleHook = Function;
 
-export class BNode<T extends typeof Component = any> implements Block<BNode> {
+export class BNode implements Block<BNode> {
   el: ChildNode | null = null;
   parentClass?: any = null;
   currentClass?: any = null;
   classTarget?: HTMLElement;
   handlers: any = null;
-  app: App;
-  fiber: Fiber | null = null;
-  component: InstanceType<T>;
-  bdom: any;
-  status: STATUS = STATUS.NEW;
+  component: ComponentClosure;
+  bdom: Block | null = null;
 
-  renderFn: Function;
   children: { [key: string]: BNode } = Object.create(null);
   slots: any = {};
   refs: any = {};
 
-  willStart: LifecycleHook[] = [];
-  willUpdateProps: LifecycleHook[] = [];
   willUnmount: LifecycleHook[] = [];
   mounted: LifecycleHook[] = [];
   willPatch: LifecycleHook[] = [];
   patched: LifecycleHook[] = [];
   destroyed: LifecycleHook[] = [];
 
-  constructor(C: T, props: any, app: App) {
+  constructor(C: Component, props: any) {
     currentNode = this;
-    this.app = app;
-    this.component = new C(props, app.env, this) as any;
-    this.renderFn = app.getTemplate(C.template).bind(null, this.component, this);
-    this.component.setup();
+    this.component = C();
   }
 
-  mountComponent(target: any): Promise<InstanceType<T>> {
-    const fiber = new MountFiber(this, target);
-    this.app.scheduler.addFiber(fiber);
-    this.initiateRender(fiber);
-    return fiber.promise.then(() => this.component);
+  mountComponent(target: any) {
+    this.bdom = this.component();
+    mountBlock(this.bdom, target);
   }
 
-  async initiateRender(fiber: Fiber | MountFiber) {
-    if (this.mounted.length) {
-      fiber.root.mounted.push(fiber);
-    }
-    const component = this.component;
-    const prom = Promise.all(this.willStart.map((f) => f.call(component)));
-    await prom;
-    if (this.status === STATUS.NEW && this.fiber === fiber) {
-      this._render(fiber);
-    }
-  }
+  // async initiateRender(fiber: Fiber | MountFiber) {
+  //   if (this.mounted.length) {
+  //     fiber.root.mounted.push(fiber);
+  //   }
+  //   const component = this.component;
+  //   const prom = Promise.all(this.willStart.map((f) => f.call(component)));
+  //   await prom;
+  //   if (this.status === STATUS.NEW && this.fiber === fiber) {
+  //     this._render(fiber);
+  //   }
+  // }
 
-  async render() {
-    if (this.fiber && !this.fiber.bdom) {
-      return this.fiber.root.promise;
-    }
-    if (!this.bdom && !this.fiber) {
-      // should find a way to return the future mounting promise
-      return;
-    }
+  // async render() {
+  //   if (this.fiber && !this.fiber.bdom) {
+  //     return this.fiber.root.promise;
+  //   }
+  //   if (!this.bdom && !this.fiber) {
+  //     // should find a way to return the future mounting promise
+  //     return;
+  //   }
 
-    const fiber = makeRootFiber(this);
-    this.app.scheduler.addFiber(fiber);
-    await Promise.resolve();
-    if (this.fiber === fiber) {
-      this._render(fiber);
-    }
-    return fiber.root.promise;
-  }
+  //   const fiber = makeRootFiber(this);
+  //   this.app.scheduler.addFiber(fiber);
+  //   await Promise.resolve();
+  //   if (this.fiber === fiber) {
+  //     this._render(fiber);
+  //   }
+  //   return fiber.root.promise;
+  // }
 
-  _render(fiber: Fiber | RootFiber) {
-    try {
-      fiber.bdom = this.renderFn();
-    } catch (e) {
-      fiber.root.error = e;
-      this.handleError(fiber);
-    }
-    fiber.root.counter--;
-  }
+  // _render(fiber: Fiber | RootFiber) {
+  //   try {
+  //     fiber.bdom = this.renderFn();
+  //   } catch (e) {
+  //     fiber.root.error = e;
+  //     this.handleError(fiber);
+  //   }
+  //   fiber.root.counter--;
+  // }
 
-  handleError(fiber: Fiber) {
-    fiber.node.app.destroy();
-  }
+  // handleError(fiber: Fiber) {
+  //   fiber.node.app.destroy();
+  // }
 
-  destroy() {
-    if (this.status === STATUS.MOUNTED) {
-      callWillUnmount(this);
-      this.bdom!.remove();
-    }
-    callDestroyed(this);
+  // destroy() {
+  //   if (this.status === STATUS.MOUNTED) {
+  //     callWillUnmount(this);
+  //     this.bdom!.remove();
+  //   }
+  //   callDestroyed(this);
 
-    function callWillUnmount(node: BNode) {
-      const component = node.component;
-      for (let cb of node.willUnmount) {
-        cb.call(component);
-      }
-      for (let child of Object.values(node.children)) {
-        if (child.status === STATUS.MOUNTED) {
-          callWillUnmount(child);
-        }
-      }
-    }
+  //   function callWillUnmount(node: BNode) {
+  //     const component = node.component;
+  //     for (let cb of node.willUnmount) {
+  //       cb.call(component);
+  //     }
+  //     for (let child of Object.values(node.children)) {
+  //       if (child.status === STATUS.MOUNTED) {
+  //         callWillUnmount(child);
+  //       }
+  //     }
+  //   }
 
-    function callDestroyed(node: BNode) {
-      const component = node.component;
-      node.status = STATUS.DESTROYED;
-      for (let child of Object.values(node.children)) {
-        callDestroyed(child);
-      }
-      for (let cb of node.destroyed) {
-        cb.call(component);
-      }
-    }
-  }
+  //   function callDestroyed(node: BNode) {
+  //     const component = node.component;
+  //     node.status = STATUS.DESTROYED;
+  //     for (let child of Object.values(node.children)) {
+  //       callDestroyed(child);
+  //     }
+  //     for (let cb of node.destroyed) {
+  //       cb.call(component);
+  //     }
+  //   }
+  // }
 
   /**
    *
@@ -147,51 +128,51 @@ export class BNode<T extends typeof Component = any> implements Block<BNode> {
    * @param owner the component in which the component was defined
    * @param parent the actual parent (may be different in case of slots)
    */
-  getChild(name: string | typeof Component, props: any, key: string, owner: any) {
-    let node: any = this.children[key];
-    let isDynamic = typeof name !== "string";
+  // getChild(name: string | typeof Component, props: any, key: string, owner: any) {
+  //   let node: any = this.children[key];
+  //   let isDynamic = typeof name !== "string";
 
-    if (node && node.status < STATUS.MOUNTED) {
-      node.destroy();
-      node = undefined;
-    }
-    if (isDynamic && node && node.component.constructor !== name) {
-      node = undefined;
-    }
+  //   if (node && node.status < STATUS.MOUNTED) {
+  //     node.destroy();
+  //     node = undefined;
+  //   }
+  //   if (isDynamic && node && node.component.constructor !== name) {
+  //     node = undefined;
+  //   }
 
-    const parentFiber = this.fiber!;
-    if (node) {
-      node.updateAndRender(props, parentFiber);
-    } else {
-      // new component
-      const C = isDynamic ? name : owner.constructor.components[name as any];
-      node = new BNode(C, props, this.app);
-      this.children[key] = node;
+  //   const parentFiber = this.fiber!;
+  //   if (node) {
+  //     node.updateAndRender(props, parentFiber);
+  //   } else {
+  //     // new component
+  //     const C = isDynamic ? name : owner.constructor.components[name as any];
+  //     node = new BNode(C, props, this.app);
+  //     this.children[key] = node;
 
-      const fiber = makeChildFiber(node, parentFiber);
-      node.initiateRender(fiber);
-    }
-    return node;
-  }
+  //     const fiber = makeChildFiber(node, parentFiber);
+  //     node.initiateRender(fiber);
+  //   }
+  //   return node;
+  // }
 
-  async updateAndRender(props: any, parentFiber: Fiber) {
-    // update
-    const fiber = makeChildFiber(this, parentFiber);
-    if (this.willPatch.length) {
-      parentFiber.root.willPatch.push(fiber);
-    }
-    if (this.patched.length) {
-      parentFiber.root.patched.push(fiber);
-    }
-    const component = this.component;
-    const prom = Promise.all(this.willUpdateProps.map((f) => f.call(component, props)));
-    await prom;
-    if (fiber !== this.fiber) {
-      return;
-    }
-    this.component.props = props;
-    this._render(fiber);
-  }
+  // async updateAndRender(props: any, parentFiber: Fiber) {
+  //   // update
+  //   const fiber = makeChildFiber(this, parentFiber);
+  //   if (this.willPatch.length) {
+  //     parentFiber.root.willPatch.push(fiber);
+  //   }
+  //   if (this.patched.length) {
+  //     parentFiber.root.patched.push(fiber);
+  //   }
+  //   const component = this.component;
+  //   const prom = Promise.all(this.willUpdateProps.map((f) => f.call(component, props)));
+  //   await prom;
+  //   if (fiber !== this.fiber) {
+  //     return;
+  //   }
+  //   this.component.props = props;
+  //   this._render(fiber);
+  // }
 
   // ---------------------------------------------------------------------------
   // Block DOM methods
@@ -203,31 +184,31 @@ export class BNode<T extends typeof Component = any> implements Block<BNode> {
   }
 
   mountBefore(anchor: ChildNode) {
-    const bdom = this.fiber!.bdom!;
-    this.bdom = bdom;
-    bdom.mountBefore(anchor);
-    if (this.parentClass) {
-      const el = this.firstChildNode();
-      if (el instanceof HTMLElement) {
-        this.addClass(el);
-      }
-      this.currentClass = this.parentClass;
-    }
-    this.status = STATUS.MOUNTED;
-    this.fiber!.appliedToDom = true;
-    this.fiber = null;
-    if (this.handlers) {
-      for (let i = 0; i < this.handlers.length; i++) {
-        const handler = this.handlers[i];
-        const eventType = handler[0];
-        const el = bdom.el!;
-        el.addEventListener(eventType, (ev: Event) => {
-          const info = this.handlers![i];
-          const [, ctx, method] = info;
-          (ctx.__owl__.component as any)[method](ev);
-        });
-      }
-    }
+    //   const bdom = this.fiber!.bdom!;
+    //   this.bdom = bdom;
+    //   bdom.mountBefore(anchor);
+    //   if (this.parentClass) {
+    //     const el = this.firstChildNode();
+    //     if (el instanceof HTMLElement) {
+    //       this.addClass(el);
+    //     }
+    //     this.currentClass = this.parentClass;
+    //   }
+    //   this.status = STATUS.MOUNTED;
+    //   this.fiber!.appliedToDom = true;
+    //   this.fiber = null;
+    //   if (this.handlers) {
+    //     for (let i = 0; i < this.handlers.length; i++) {
+    //       const handler = this.handlers[i];
+    //       const eventType = handler[0];
+    //       const el = bdom.el!;
+    //       el.addEventListener(eventType, (ev: Event) => {
+    //         const info = this.handlers![i];
+    //         const [, ctx, method] = info;
+    //         (ctx.__owl__.component as any)[method](ev);
+    //       });
+    //     }
+    //   }
   }
 
   moveBefore(anchor: ChildNode) {
@@ -248,42 +229,42 @@ export class BNode<T extends typeof Component = any> implements Block<BNode> {
   }
 
   patch() {
-    this.bdom!.patch(this!.fiber!.bdom!);
-    if (this.parentClass) {
-      const el = this.firstChildNode() as HTMLElement;
-      if (el === this.classTarget) {
-        const prev = toClassObj(this.currentClass);
-        const next = toClassObj(this.parentClass);
-        for (let c in prev) {
-          if (!(c in next)) {
-            el.classList.remove(c);
-          }
-        }
-        // add classes
-        for (let c in next) {
-          if (!(c in prev)) {
-            el.classList.add(c);
-          }
-        }
-        this.currentClass = next;
-      } else {
-        if (el && this.classTarget) {
-          this.removeClass(this.classTarget);
-          this.addClass(el as any);
-        } else if (el) {
-          this.addClass(el as any);
-        } else {
-          this.removeClass(this.classTarget!);
-          this.classTarget = undefined;
-        }
-      }
-    }
-    this.fiber!.appliedToDom = true;
-    this.fiber = null;
+    //   this.bdom!.patch(this!.fiber!.bdom!);
+    //   if (this.parentClass) {
+    //     const el = this.firstChildNode() as HTMLElement;
+    //     if (el === this.classTarget) {
+    //       const prev = toClassObj(this.currentClass);
+    //       const next = toClassObj(this.parentClass);
+    //       for (let c in prev) {
+    //         if (!(c in next)) {
+    //           el.classList.remove(c);
+    //         }
+    //       }
+    //       // add classes
+    //       for (let c in next) {
+    //         if (!(c in prev)) {
+    //           el.classList.add(c);
+    //         }
+    //       }
+    //       this.currentClass = next;
+    //     } else {
+    //       if (el && this.classTarget) {
+    //         this.removeClass(this.classTarget);
+    //         this.addClass(el as any);
+    //       } else if (el) {
+    //         this.addClass(el as any);
+    //       } else {
+    //         this.removeClass(this.classTarget!);
+    //         this.classTarget = undefined;
+    //       }
+    //     }
+    //   }
+    //   this.fiber!.appliedToDom = true;
+    //   this.fiber = null;
   }
 
   beforeRemove() {
-    visitRemovedNodes(this);
+    // visitRemovedNodes(this);
   }
 
   remove() {
@@ -292,18 +273,18 @@ export class BNode<T extends typeof Component = any> implements Block<BNode> {
   }
 }
 
-function visitRemovedNodes(node: BNode) {
-  if (node.status === STATUS.MOUNTED) {
-    const component = node.component;
-    for (let cb of node.willUnmount) {
-      cb.call(component);
-    }
-  }
-  for (let child of Object.values(node.children)) {
-    visitRemovedNodes(child);
-  }
-  node.status = STATUS.DESTROYED;
-  if (node.destroyed.length) {
-    __internal__destroyed.push(node);
-  }
-}
+// function visitRemovedNodes(node: BNode) {
+//   if (node.status === STATUS.MOUNTED) {
+//     const component = node.component;
+//     for (let cb of node.willUnmount) {
+//       cb.call(component);
+//     }
+//   }
+//   for (let child of Object.values(node.children)) {
+//     visitRemovedNodes(child);
+//   }
+//   node.status = STATUS.DESTROYED;
+//   if (node.destroyed.length) {
+//     __internal__destroyed.push(node);
+//   }
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,63 @@
-import { App } from "./app";
-import { Component } from "./component";
-import { getCurrent } from "./b_node";
+import { Block, Blocks as BaseBlocks } from "./bdom";
+import { BNode } from "./b_node";
+import { compileTemplate, Template } from "./compiler";
+import { UTILS } from "./template_utils";
 
-export { App, Component };
+// export { App, Component };
 
-export async function mount<T extends typeof Component>(
-  C: T,
-  target: HTMLElement
-): Promise<InstanceType<T>> {
-  const app = new App(C);
-  return app.mount(target);
-}
+// export function useComponent(): Component {
+//   const current = getCurrent();
+//   return current!.component;
+// }
 
-export function useComponent(): Component {
-  const current = getCurrent();
-  return current!.component;
-}
-
-export { status } from "./status";
-export { Portal } from "./misc/portal";
-export { Memo } from "./misc/memo";
-export { xml } from "./tags";
-export { useState } from "./reactivity";
-export { useRef } from "./refs";
-export { EventBus } from "./event_bus";
-
-export {
-  onWillStart,
-  onMounted,
-  onWillUnmount,
-  onWillUpdateProps,
-  onWillPatch,
-  onPatched,
-  onRender,
-  onDestroyed,
-} from "./lifecycle_hooks";
+// export { EventBus } from "./event_bus";
+// export {
+//   onDestroyed, onMounted, onPatched,
+//   onRender, onWillPatch, onWillStart, onWillUnmount,
+//   onWillUpdateProps
+// } from "./lifecycle_hooks";
+// export { Memo } from "./misc/memo";
+// export { Portal } from "./misc/portal";
+// export { useState } from "./reactivity";
+// export { useRef } from "./refs";
+// export { status } from "./status";
+// // export { xml } from "./tags";
 
 export const __info__ = {};
+
+// -----------------------------------------------------------------------------
+// Tag xml
+// -----------------------------------------------------------------------------
+
+const Blocks = {
+  ...BaseBlocks,
+  BNode,
+};
+
+export function xml(strings: TemplateStringsArray, ...args: any[]) {
+  const value = String.raw(strings, ...args);
+  return compileTemplate(value)(Blocks, UTILS);
+}
+
+// -----------------------------------------------------------------------------
+// Render
+// -----------------------------------------------------------------------------
+
+export function render(template: Template, context: any = {}): Block {
+  return template(context, {});
+}
+
+export type ComponentClosure = () => Block;
+
+export type Component = () => ComponentClosure;
+
+export async function mount(C: Component, target: HTMLElement): Promise<void> {
+  if (!(target instanceof HTMLElement)) {
+    throw new Error("Cannot mount component: the target is not a valid DOM element");
+  }
+  if (!document.body.contains(target)) {
+    throw new Error("Cannot mount a component on a detached dom node");
+  }
+  const node = new BNode(C, {});
+  return node.mountComponent(target);
+}
